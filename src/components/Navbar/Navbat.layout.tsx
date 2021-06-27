b25e208fce0a44a5d6058f4ad205419854e67d0a
OleinikovArtem <artem.oleinikov.web@gmail.com>
2021-06-27 17:47:06 +0300

    chenged Animation time

--- src/components/Navbar/Navbat.layout.tsx
+++ src/components/Navbar/Navbat.layout.tsx
@@ -6,29 +6,29 @@ const NavbarLayout: FC = () => (
 		<div className="navigation__lang code">EN</div>
 		<menu className="navigation__menu">
 			<li className="navigation__menu-item">
-				<Link href="/" passHref prefetch>
+				<Link href="/" passHref>
 					<a className="navigation__menu-link" href="">
 						Home
 					</a>
 				</Link>
 			</li>
 			<li className="navigation__menu-item">
-				<Link href="/about" passHref prefetch>
+				<Link href="/about" passHref>
 					<a className="navigation__menu-link">About Me</a>
 				</Link>
 			</li>
 			<li className="navigation__menu-item">
-				<Link href="/skills" passHref prefetch>
+				<Link href="/skills" passHref>
 					<a className="navigation__menu-link">Skills</a>
 				</Link>
 			</li>
 			<li className="navigation__menu-item">
-				<Link href="/portfolio" passHref prefetch>
+				<Link href="/portfolio" passHref>
 					<a className="navigation__menu-link">Portfolio</a>
 				</Link>
 			</li>
 			<li className="navigation__menu-item">
-				<Link href="/experience" passHref prefetch>
+				<Link href="/experience" passHref>
 					<a className="navigation__menu-link">Experience</a>
 				</Link>
 			</li>
